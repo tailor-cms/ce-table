b7e6faa083654bd60c6910eb6670ec8e0464e3a2
rkusan00 <renato.kusanovic@gmail.com>
2024-10-20 17:57:11 +0200

    Update names and structure

--- packages/edit/src/index.ts
+++ packages/edit/src/index.ts
@@ -1,5 +1,5 @@
-import baseManifest from 'tce-manifest';
-import type { ElementManifest } from 'tce-manifest';
+import baseManifest from '@tailor-cms/ce-table-manifest';
+import type { ElementManifest } from '@tailor-cms/ce-table-manifest';
 
 import Edit from './components/Edit.vue';
 import SideToolbar from './components/SideToolbar.vue';
