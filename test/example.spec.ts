3af1bc1c335bb404cd231694bc374790ca7c0eca
rkusan00 <renato.kusanovic@gmail.com>
2025-02-12 10:22:33 +0100

    Update tests

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -7,19 +7,13 @@ test.beforeEach(async ({ page }) => {
 test('Renders Edit component', async ({ page }) => {
   const editFrame = page.frameLocator('#editPanel>iframe')
   await expect(editFrame.getByText('Authoring component')).toBeVisible();
-  const EDIT_COPY = 'Edit version of the content element';
-  await expect(editFrame.getByText(EDIT_COPY)).toBeVisible();
-  const TOP_TOOLBAR_COPY = 'Top toolbar';
-  await expect(editFrame.getByText(TOP_TOOLBAR_COPY)).toBeVisible();
-  const SIDE_TOOLBAR_COPY = 'Side toolbar';
-  await expect(editFrame.getByText(SIDE_TOOLBAR_COPY)).toBeVisible();
+  await expect(editFrame.getByText('Top toolbar')).not.toBeVisible();
+  await expect(editFrame.getByText('Side toolbar')).not.toBeVisible();
 });
 
 test('Renders Display component', async ({ page }) => {
   const displayFrame = page.frameLocator('#displayPanel>iframe')
   await expect(displayFrame.getByText('End-user component')).toBeVisible();
-  const DISPLAY_COPY = 'Display version of the content element';
-  await expect(displayFrame.getByText(DISPLAY_COPY)).toBeVisible();
 });
 
 test('Renders server state panel', async ({ page }) => {
@@ -28,7 +22,6 @@ test('Renders server state panel', async ({ page }) => {
   const authoringTab = bottomPanel.getByRole('tab', { name: 'Authoring history' });
   const userStateTab = bottomPanel.getByRole('tab', { name: 'End-user state history' });
   await expect(authoringTab).toBeVisible();
-  await expect(bottomPanel.locator('pre').getByText('state')).toBeVisible();
   await expect(userStateTab).toBeVisible();
   await authoringTab.click();
   for (const prop of properties) {
